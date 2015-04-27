54d79f4706c4293658236dc2790bce52850d8ee7
Bruno <sautron.brunojj@gmail.com>
2015-04-27 02:10:21 +0200

    ghauhf

--- computor.js
+++ computor.js
@@ -1,29 +1,15 @@
-function cleanArray (array) {
-	var n = [];
-	for (var i = 0; i < array.length; i++) {
-		if (array[i] != " ") n.push (array[i]);
-		else if (i == 0) n.push (array[i])
-	}	
-	return (n);
-}
-
-
-
 var Polynom = require ("./polynom"),
 	Parser = require ("./parser"),
 	Solver = require ("./solver"),
-
 	arg = (function () {
-		var argv = process.argv;
+		var argv = process.argv,
+			noSpace = argv[2].replace(/[\s]+/g, ""),
+			equal = noSpace.split(/=/);
 
 		if (argv.length != 3) {
 			console.log ("Error: Wrong number of arguments");
 			process.exit(1);
 		}
-
-		var noSpace = argv[2].replace(/[\s]+/g, ""),
-			equal = noSpace.split(/=/);
-
 
 		if (equal[0][0] != "-" && equal[0][0] != "+")
 			equal[0] = "+" + equal[0];
@@ -41,35 +27,19 @@ var Polynom = require ("./polynom"),
 							.replace(/X\^/g, "x^")
 							.replace(/X/g, "x^1")
 							.replace(/x/g, "X");
-		// console.log(noSpace);
-		
-		// var test = noSpace.split(/[\+\-=]/g).filter(notNull),
-		// 	signs = noSpace.match(/[\+\-=]/g);
 
-		// console.log(test);
-		// console.log(signs);
-
-		// var finale = [];
-		// for (var i = 0; i < test.length; i++) {
-		// 	if (isNaN(test[i])) 
-		// 		finale.push (test[i]);
-		// 	else if (!isNaN(test[i]) && test[i].trim().length != 0) {
-		// 		finale.push (test[i] + " * X^0")
-		// 	}
-		// 	if (signs[i])
-		// 		finale.push(signs[i]);
-		// }
-		// return finale.join(" ");
 		return (noSpace);
 	})(),
 
 	p = new Polynom(arg);
 
+
+
 p.reduce();
 p.merge();
 p.degreeMax();
-console.log("Polynomial degree: " + p.degree);
 console.log("Reduced form: " + p.goodStr);
+console.log("Polynomial degree: " + p.degree);
 // console.log(p.str);
 // p.splitEqual();
 // Parser.reduce(p);
--- polynom.js
+++ polynom.js
@@ -2,23 +2,12 @@ function Polynom (str) {
 	this.str = str;
 	this.goodStr;
 	this.degree = 0;
-
-	// this.splitNoSpace = function () {
-		
-	// 	this.str = this.str.replace(/\s/g, "");
-	// 	this.str = this.str.replace(/-/g, "+-");
-	// 	this.str = this.str.replace(/\*\+/g, "*");
-
-	// 	var array = this.str.split("+");
-	// 	return array;
-	// }
 }
 
 Polynom.prototype.reduce = function () {
-	// console.log("ICI");
-	// console.log(this.str);
 	var elem = this.str.split(/[\+\-=]/g),
 		signe = this.str.match(/[\-\+=]/g);
+
 	elem = elem.filter(function (element) {
 		if (element != '')
 			return true;
@@ -26,33 +15,24 @@ Polynom.prototype.reduce = function () {
 		});
 
 	for (i = 0; elem[i]; i++) {
-		var puiss = elem[i].match(/[\^][0-9]+/g);
-		// console.log(puiss);
+		var puiss = elem[i].match(/[\^][0-9]+/g),
+			noPuiss = eval(elem[i].replace(/[\^][0-9]+/g, "")
+								.replace(/X/g, "")
+								.replace(/\*$/, ""));
+
 		if (puiss) {
 			for (j = 0; puiss[j]; j++) {
 				puiss[j] = puiss[j].replace(/\^/, "");
 			}
 			puiss = eval(puiss.join("\+"));
-		} else {
-			puiss = "0";
 		}
-
-		var	noPuiss = eval(elem[i].replace(/[\^][0-9]+/g, "")
-								.replace(/X/g, "")
-								.replace(/\*$/, ""));
+		else
+			puiss = "0";
 		elem[i] = signe[i] + noPuiss + "X^"  + puiss;
 	}
 
-	// console.log(elem.join(""));
-
-	// console.log("PD form: " + elem.join("")
-	// 									.replace(/\-/g, " - ")
-	// 									.replace(/\+/g, " + ")
-	// 									.replace(/=/g, " = ")
-	// 									.replace(/X/g, " * X"));
 	this.str = elem.join("");
 	return (elem.join(""));
-	// console.log(signe);
 }
 
 
@@ -62,15 +42,16 @@ Polynom.prototype.merge = function () {
 			return true;
 		return false;
 		}),
-		signe = this.str.match(/[\+\-=]/g);
-
-	var maxDegree = 0;
+		signe = this.str.match(/[\+\-=]/g),
+		maxDegree = 0,
+		poly = [],
+		realPoly= [];
+		
 	for (i = 0; array[i]; i++) {
 		if (array[i].split(/X\^/)[1] > maxDegree)
 			maxDegree = array[i].split(/X\^/)[1];
 	}
-	var poly = [],
-		realPoly= [];
+
 	for (i = 0; i <= maxDegree; i++) {
 		poly.push(0);
 		poly[i] = 0;
@@ -86,6 +67,7 @@ Polynom.prototype.merge = function () {
 		if (parseFloat(poly[i]) != 0)
 			realPoly.push(poly[i] + "X^" + i);
 	}
+
 	this.str = realPoly.join("+")
 						.replace(/\+\-/g, "-") + "=0";
 	this.goodStr = this.str.replace(/\-/g, " - ")
@@ -100,41 +82,13 @@ Polynom.prototype.degreeMax = function () {
 			return true;
 		return false;
 		});
+
 	this.degree = 0;
+	
 	for (i = 0; array[i]; i++) {
 		if (array[i].split(/X\^/)[1] > this.degree)
 			this.degree = array[i].split(/X\^/)[1];
 	}
-
-
-}
-
-Polynom.prototype.splitEqual = function () {
-		var array = this.str.split("=");
-		if (array.length != 2) {
-			console.log ("Wrong number of '='");
-			process.exit(1);
-		}
-
-		array[1] = array[1].replace(/\s/g, "")
-		array[1] = array[1].replace(/(?!^)-/g, "+-")
-		array[1] = array[1].replace(/\*\+/g, "*");
-		var split = array[1].split("+");
-		for (var i = 0; i < split.length; i++) {
-			if (split[i][0] == '-') split[i] = "+" + split[i].slice(1);
-			else split[i] = "-" + split[i];
-		}
-		
-		array[1] = split.join("");
-		this.str = array[0] + array[1];
-}
-
-Polynom.prototype.write = function() {
-	console.log (this.str);
-};
-
-Polynom.replaceMinus = function () {
-	this.str = this.str.replace(/-/g, "+-");
 }
 
 module.exports = Polynom;
