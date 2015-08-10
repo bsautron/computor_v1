a0aef564ab4afa3555a068a27d0da0ec0169274b
Bruno SAUTRON <bsautron@e2r6p18.42.fr>
2015-08-11 00:31:30 +0200

    sleiughrsefg

--- polynom.js
+++ polynom.js
@@ -1,44 +1,114 @@
 function Polynom (str) {
 	this.str = str;
+	this.goodStr;
 	this.degree = 0;
-
-	this.splitNoSpace = function () {
-		
-		this.str = this.str.replace(/\s/g, "");
-		this.str = this.str.replace(/-/g, "+-");
-		this.str = this.str.replace(/\*\+/g, "*");
-
-		var array = this.str.split("+");
-		return array;
+	this.solution = {
+		"x0" : "",
+		"x1" : "",
+		"x2" : "",
+	};
+	this.coeff = {
+		"a" : 0,
+		"b" : 0,
+		"c" : 0,
 	}
 }
 
-Polynom.prototype.splitEqual = function () {
-		var array = this.str.split("=");
-		if (array.length != 2) {
-			console.log ("Wrong number of '='");
-			process.exit(1);
-		}
+Polynom.prototype.reduce = function () {
+	var elem = this.str.split(/[\+\-=]/g),
+		signe = this.str.match(/[\-\+=]/g);
+	
+	elem.shift();
 
-		array[1] = array[1].replace(/\s/g, "")
-		array[1] = array[1].replace(/(?!^)-/g, "+-")
-		array[1] = array[1].replace(/\*\+/g, "*");
-		var split = array[1].split("+");
-		for (var i = 0; i < split.length; i++) {
-			if (split[i][0] == '-') split[i] = "+" + split[i].slice(1);
-			else split[i] = "-" + split[i];
+	for (i = 0; elem[i]; i++) {
+		var puiss = elem[i].match(/[\^][0-9]+/g),
+			noPuiss = elem[i].replace(/[\^][0-9]+/g, "")
+								.replace(/X/g, "")
+								.replace(/\*$/, "")
+								.split(/\*/g),
+			nn = 1;
+			
+		for (j = 0; noPuiss[j]; j++) {
+			nn *= noPuiss[j];
 		}
+		noPuiss = nn;
 		
-		array[1] = split.join("");
-		this.str = array[0] + array[1];
+
+		if (puiss) {
+			var pp = 0;
+			for (j = 0; puiss[j]; j++) {
+				puiss[j] = puiss[j].replace(/\^/, "");
+				pp += parseFloat(puiss[j]);
+			}
+			puiss = pp.toString();
+		}
+		else
+			puiss = "0";
+		elem[i] = signe[i] + noPuiss + "X^"  + puiss;
+	}
+
+	this.str = elem.join("");
 }
 
-Polynom.prototype.write = function() {
-	console.log (this.str);
-};
 
-Polynom.replaceMinus = function () {
-	this.str = this.str.replace(/-/g, "+-");
+Polynom.prototype.merge = function () {
+	var array = this.str.split(/[\+\-=]/g),
+		signe = this.str.match(/[\+\-=]/g),
+		maxDegree = 0,
+		poly = [],
+		realPoly= [];
+	array.shift();
+	
+	for (i = 0; array[i]; i++) {
+		if (array[i].split(/X\^/)[1] > maxDegree)
+			maxDegree = array[i].split(/X\^/)[1];
+	}
+
+	for (i = 0; i <= maxDegree; i++) {
+		poly.push(0);
+		poly[i] = 0;
+		for (j = 0; array[j]; j++) {
+			if (array[j].split(/\^/g)[1] == i) {
+				if (signe[j] == "-")
+					poly[i] -= parseFloat(array[j].split(/X/g)[0]);
+				else
+					poly[i] += parseFloat(array[j].split(/X/g)[0]);
+			}
+
+		}
+		if (parseFloat(poly[i]) != 0)
+			realPoly.push(poly[i] + "X^" + i);
+	}
+
+	if (maxDegree == 0 && realPoly.length != 0) {
+		console.log("I can't resolve that!")
+		process.exit(1);
+	}
+
+	this.str = realPoly.join("+")
+						.replace(/\+\-/g, "-") + "=0";
+	this.goodStr = this.str.replace(/\-/g, " - ")
+							.replace(/\+/g, " + ")
+							.replace(/=/g, " = ")
+							.replace(/X/g, " * X")
+							.replace(/\ \*\ X\^0/g, "")
+							.replace(/X\^1/g, "X");
+	if (this.str[0] !== '-')
+		this.str = "+" + this.str;
+}
+
+Polynom.prototype.degreeMax = function () {
+	var array = this.str.split(/[\+\-=]/g);
+	array.shift();
+
+	this.degree = 0;
+
+	for (i = 0; array[i]; i++) {
+		if (array[i].split(/X\^/)[1] > this.degree)
+			this.degree = array[i].split(/X\^/)[1];
+	}
+	if (this.degree == 0)
+		this.goodStr = "I can't reduce that!";
 }
 
 module.exports = Polynom;
--- solver.js
+++ solver.js
@@ -1,99 +1,208 @@
-
-function sqrt (number) {
-  return Math.sqrt(number);
+function ftSqrt(a) {
+  for (i = 0; i < a / 2 + 1; i++) {
+    if (i * i == a)
+      return (i.toString());
+  }
+  return ("√" + a.toString());
 }
 
-function format (split) {
+function pgcd(n1, n2) {
+  if (n1 < 0)
+    n1 = -n1;
+  if (n2 < 0)
+    n2 = -n2;
+  while (n1 * n2 != 0) {
+    if (n1 > n2)
+      n1 -= n2;
+    else 
+      n2 -= n1;
+  }
+  if (n1 == 0) return (n2);
+  return (n1);
+}
+
+function format(split) {
   for (var i = 0; i < split.length; i++) {
     if (split[i] == "-") split[i + 1] = "-" + split[i + 1];
   }
   return (split);
 }
 
-function Solver (options) {
+function Solver() {
 
-  this.solveForOne = function (polynom) {
-    var split = polynom.str.split(" ");
-
-    var right = (function () {
-      var ret = "";
-      for (var i = 0; i < split.length; i++) {
-        if (split[i].indexOf("X^0") > -1) {
-          ret = split[i][0] == "-" ? split[i].slice(1) : "-" + split[i];
-          split.splice(i, 1);
-        }
-      }
-      return (ret);
-    })().replace("X^0", "");
-
-    var left = (function () {
-      var ret = "";
-      for (var i = 0; i < split.length; i++) {
-        if (split[i].indexOf("X^1") > -1)
-          ret = format(split)[i];
-      }
-      return (ret);
-    })().replace("X^1", "");
-
-    if (right[0] == '-' && left[0] == '-') {
-      left = left.slice(1);
-      right = right.slice(1);
-    }
-    return right + "/" + left;
+  this.solveForZero = function(polynom) {
+    console.log("Solutions are:");
+    console.log("X ∈ R");
   }
 
+  this.solveForOne = function(polynom) {
+    var array = polynom.str.split(/[\+\-=]/g),
+        signe = polynom.str.match(/[\-\+=]/g),
+        neg = false,
+        pg,
+        nume,
+        denom,
+        q = "";
+    array.shift();
+  
+    nume = parseFloat(array[0]);
+    denom = parseFloat(array[1]);
+    if (array.length == 3) {
+      if (signe[0] === '+')
+        neg = !neg;
+      if (signe[1] === '-')
+        neg = !neg;
+      if (nume == parseInt(array[0])
+          && denom == parseInt(array[1])
+        	&& ( pg = pgcd(nume, denom) )) {
+        nume /= pg;
+        denom /= pg;
+        q = nume + ((denom != 1) ? " / " + denom : "");
+        if (neg)
+          q = "-" + q;  
+      }
+      else
+        q = (nume / denom).toString();
+    }
+    else
+      q = "0";
+
+    console.log("The solution is:");
+    console.log(q);
+    polynom.solution["x0"] = q; 
+  }
+
+
   this.solveForTwo = function (polynom) {
-    var split = polynom.str.split(" ");
+    var array = polynom.str.split(/[\-\+=]/g),
+        signe = polynom.str.match(/[\-\+=]/g),
+        sp = [],
+        a = "0",
+        b = "0",
+        c = "0",
+        delta;
 
-    var a = (function () {
-      var ret = "";
-      for (var i = 0; i < split.length; i++) {
-        if (split[i].indexOf("X^2") > -1)
-          ret = format(split)[i];
+    array.shift();
+  
+    for (var i = 0; array[i]; i++) {
+      sp = array[i].split("X^");
+      if (sp[1] == 2) a = signe[i] + sp[0];
+      if (sp[1] == 1) b = signe[i] + sp[0];
+      if (sp[1] == 0) c = signe[i] + sp[0];
+    }
+    
+    polynom.coeff["a"] = a;
+    polynom.coeff["b"] = b;
+    polynom.coeff["c"] = c;
+
+    delta = (b * b - 4 * a * c).toString();
+    console.log("𝚫 = " + delta);
+    
+    if (delta > 0) {
+      var racineDelta = ftSqrt(delta),
+          n1 = -b + ((racineDelta[0] == '√') ? " + " + racineDelta : parseInt(racineDelta)),
+          n2 = -b + ((racineDelta[0] == '√') ? " - " + racineDelta : parseInt("-" + racineDelta)),
+          d1 = 2 * a,
+          d2 = 2 * a,
+          x1,
+          x2,
+          pg,
+          neg = false;
+          
+      if (parseFloat(racineDelta) == parseInt(racineDelta)) {
+        if (pg = pgcd(n1, d1)) {
+          n1 /= pg;
+          d1 /= pg;
+        }
+        if (n1 < 0)
+          neg = !neg;
+        if (d1 < 0)
+          neg = !neg;
+        x1 = (neg ? "-" : "") + ((n1 < 0) ? -n1 : n1);
+        if (d1 != 1 && d1 != -1)
+          x1 += " / " + ((d1 < 0) ? -d1 : d1);
+        
+        neg = false;
+        if (pg = pgcd(n2, d2)) {
+          n2 /= pg;
+          d2 /= pg;
+        }
+        if (n2 < 0)
+          neg = !neg;
+        if (d2 < 0)
+          neg = !neg;
+        x2 = (neg ? "-" : "") + ((n2 < 0) ? -n2 : n2)
+        if (d2 != 1 && d2 != -1)
+          x2 += " / " + ((d2 < 0) ? -d2 : d2);
       }
-      return parseInt(ret.replace("X^2", ""));
-    })();
-
-    var b = (function () {
-      var ret = "";
-      for (var i = 0; i < split.length; i++) {
-        if (split[i].indexOf("X^1") > -1)
-          ret = format(split)[i];
+      else {
+        x1 = ((racineDelta[0] == '√') ? "(" + n1 + ") / " : n1 + " / ") + d1;
+        x2 = ((racineDelta[0] == '√') ? "(" + n2 + ") / " : n2 + " / ") + d2;
       }
-      return parseFloat(ret.replace("X^1", ""));
-    })();
-
-    var c = (function () {
-      var ret = "";
-      for (var i = 0; i < split.length; i++) {
-        if (split[i].indexOf("X^0") > -1)
-          ret = format(split)[i];
+      x0 = (b / 2 * a);
+      console.log("Solutions are: ");
+      console.log(x1);
+      console.log(x2);
+      polynom.solution["x0"] = x0;
+      polynom.solution["x1"] = x1;
+      polynom.solution["x2"] = x2;
+    }
+    else if (delta == 0) {
+      var x0,
+          n0 = -b,
+          d0 = 2 * a,
+          pg,
+          neg = false;
+          
+      if (n0 < 0) {
+        n0 = -n0;
+        neg = !neg;
       }
-      return parseFloat(ret.replace("X^0", "").replace(/-+/g, "-"));
-    })();
+      if (d0 < 0) {
+        d0 = -d0;
+        neg = !neg;
+      }
+      if (pg = pgcd(n0, d0))
+      {
+        n0 /= pg;
+        d0 /= pg;
+      }
+      if (b == 0)
+        x0 = "0";
+      else if (d0 == 1)
+        x0 = n0;
+      else
+        x0 = ((neg) ? "- " : "") + n0 + " / " + d0;
+      console.log("Solution is: ");
+      console.log(x0);
+      polynom.solution["x0"] = x0;
+      
+    }
+    else {
+      racineDelta = delta.replace(/\-/g, "i√");
+      var x1 = ((-b) ? "( " + -b + " + " : "") + racineDelta + ((-b) ? " )" : "") + " / " + (2 * a),
+          x2 = ((-b) ? "( " + -b : "") + " - " + racineDelta + ((-b) ? " )" : "") + " / " + (2 * a);
+      console.log("Solutions complexe are: ");
+      console.log(x1);
+      console.log(x2);
+      polynom.solution["x1"] = x1;
+      polynom.solution["x2"] = x2;
+    }
 
-    var delta = (function () {
-      return b * b - 4 * a * c;
-    })();
+  }
 
-    if (delta < 0)
-      return "aucune solution";
-    else if (delta == 0)
-      return -b / 2 * a;
-
-    console.log ("delta " + delta);
-
-    var x1 = (-b - sqrt(delta)) / 2 * a;
-    var x2 = (-b + sqrt(delta)) / 2 * a;
-
-    return x1 + " et " + x2;
-
+  this.solveForOver = function(polynom) {
+    console.log("The polynomial degree is stricly greater than 2, I can't solve.");
   }
 
   this.solve = function (polynom) {
-    if (polynom.degree == 1) return this.solveForOne (polynom)   
-    if (polynom.degree == 2) return this.solveForTwo (polynom)   
+    if (polynom.degree == 0) this.solveForZero(polynom);
+    if (polynom.degree == 1) this.solveForOne(polynom);  
+    if (polynom.degree == 2) this.solveForTwo(polynom);
+    if (polynom.degree >= 3) this.solveForOver(polynom);
   }
 }
 
-module.exports = new Solver ({});
+module.exports = new Solver();
+
+
